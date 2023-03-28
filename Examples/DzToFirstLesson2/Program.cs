6a80a32210528ad9d40a2777acbcd3c34bba9139
Fedor Abramov <fedo2002abro@gmail.com>
2023-03-28 23:47:36 +0300

    Half from second DZ

--- Examples/DzToFirstLesson2/Program.cs
+++ Examples/DzToFirstLesson2/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿System.Console.WriteLine("Введите первое число: ");
+int firstNum = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите число второе: ");
+int secondNum = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите третье число:");
+int third = int.Parse(Console.ReadLine());
+
+//int Max()
+
+int[] array = {firstNum, secondNum, third};
+
+/*
+if (firstNum > secondNum){
+
+    if (firstNum > third){
+
+        System.Console.Write("Самое большое число: ");
+        Console.Write(firstNum);
+    
+    }else{
+    System.Console.Write("Самое большое число: ");
+    Console.Write(third);
+}
+
+}else{
+    System.Console.Write("Самое большое число: ");
+    Console.Write(secondNum);
+}
+*/
