1c1bda83f58dc5e600c95833afefb4690ad4728b
Fedor Abramov <fedo2002abro@gmail.com>
2023-03-29 20:55:00 +0300

    Finished DzToFirstLesson3

--- Examples/DzToFirstLesson3/Program.cs
+++ Examples/DzToFirstLesson3/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+System.Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0) {
+    System.Console.Write(number);
+    System.Console.WriteLine(" четное число");
+}else{
+    System.Console.Write(number);
+    System.Console.WriteLine(" не четное число");
+}
