21732a0c4c5d88a8ef9e290c5fcd76dcb70faaaf
Fedor Abramov <fedo2002abro@gmail.com>
2023-04-01 14:43:01 +0300

    Added finised DzToSecondLesson1

---
+++ Examples/DzToSecondLesson1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+int MyNum(){
+
+    System.Console.Write("Введите трехзначное число: ");
+    int n = int.Parse(System.Console.ReadLine());
+    return n;
+
+}
+
+int N = MyNum();
+
+int n = N % 10;
+int Num = N - n;
+int Numers = (Num % 100) / 10;
+
+System.Console.WriteLine(Numers);
