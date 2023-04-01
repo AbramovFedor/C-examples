74bb0d694e028027e10cf9faf8a8fc6b97f6e8bb
Fedor Abramov <fedo2002abro@gmail.com>
2023-04-01 23:14:21 +0300

    Added finished DzToSecondLesson3

---
+++ Examples/DzToSecondLesson3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+int WeekDay(){
+    System.Console.WriteLine("Введите номер дня недели: ");
+    int a = int.Parse(System.Console.ReadLine());
+    return a;
+}
+
+int NumDay = WeekDay();
+
+if (NumDay <= 5){
+    System.Console.WriteLine("Не выходной");
+}else if(NumDay < 8 && NumDay > 5){
+    System.Console.WriteLine("Выходной!");
+}else{
+    System.Console.WriteLine("В неделе только семь дней!");
+}
