6a86387d6876108788f7cf4b5968154ad6f59f22
Fedor Abramov <fedo2002abro@gmail.com>
2023-04-01 13:00:37 +0300

    Added finished DzToFirstLesson4

--- Examples/DzToFirstLesson4/Program.cs
+++ Examples/DzToFirstLesson4/Program.cs
@@ -1,1 +1,23 @@
 ﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int AllTwoNumbers(){
+
+    System.Console.Write("Введите число: ");
+    int numb = int.Parse(System.Console.ReadLine());
+    return numb;
+
+}
+
+int Num = AllTwoNumbers();
+
+while(Num != 0){
+
+    if (Num % 2 == 0) {
+        System.Console.Write($"{Num} ");
+        //System.Console.WriteLine(" четное число");
+        Num--;
+    }else{
+        Num--;
+    }
+
+}
