1a70babce0e84b48ee200ac1e9d274e16c675246
Fedor Abramov <fedo2002abro@gmail.com>
2023-04-05 21:39:21 +0300

    Finished Dz-002

---
+++ Examples/DzToLesson4/DzToLesson4-002/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+double ReadQoute(string qoute ){
+    System.Console.Write($"Введите номер {qoute}: ");
+    double x = double.Parse(System.Console.ReadLine());
+    return x;
+}
+
+double x1 = ReadQoute("x1");
+
+double y1 = ReadQoute("y1");
+
+double z1 = ReadQoute("z1");
+
+double x2 = ReadQoute("x2");
+
+double y2 = ReadQoute("y2");
+
+double z2 = ReadQoute("z2");
+
+double Sqrt(double x1, double y1, double z1, double x2, double y2, double z2){
+
+    double C1 = Math.Round(Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2) + Math.Pow((z2 - z1),2)), 2);
+
+    return C1;
+
+}
+
+double C1 = Sqrt(x1, y1, z1, x2, y2, z2);
+
+System.Console.WriteLine($"Координаты точки: {C1}");
