65d5d59e054ec3312f1f3a723fb13f8c13ccb5c2
Fedor Abramov <fedo2002abro@gmail.com>
2023-04-05 22:32:27 +0300

    Added finished Dz-003

---
+++ Examples/DzToLesson4/DzToLesson4-003/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int a = Number();
+
+int result = Mult(a);
+
+int Number(){
+    System.Console.Write("Введите число: ");
+    int a = int.Parse(System.Console.ReadLine());
+    return a;
+}
+
+int Mult(int a){
+    int b = 1;
+    int result = a - a + 1;
+    for (int i = 1; i <= a; i++){
+        result = b * b * b;
+        System.Console.WriteLine(result);
+        result++;
+        b++;
+    }
+    return result;
+}
