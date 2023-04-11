a4556212ef688b74a77dc1b43b291bb49bdfc648
Fedor Abramov <fedo2002abro@gmail.com>
2023-04-11 20:18:10 +0300

    Finished and added DzToRealLesson-003

--- Examples/DzToRealLesson4/003/Program.cs
+++ Examples/DzToRealLesson4/003/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int [] CreateArray()
+{
+int [] numbers = new int [8];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(1,10);
+}
+return numbers;
+}
+
+void PrintArray(int [] numbers){
+    System.Console.Write("[");
+    for (int i = 0; i < numbers.Length -1; i++){
+        System.Console.Write(numbers[i] + "; ");
+    }
+    System.Console.WriteLine(numbers[numbers.Length -1] + "]");
+}
+
+
+int [] numbers = CreateArray();
+PrintArray(numbers);
