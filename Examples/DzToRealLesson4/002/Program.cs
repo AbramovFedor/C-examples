752b64fdea452b04877ff11f407dccf64e41f9f8
Fedor Abramov <fedo2002abro@gmail.com>
2023-04-11 19:59:55 +0300

    Finished and added DzToRealLesson4 002

--- Examples/DzToRealLesson4/002/Program.cs
+++ Examples/DzToRealLesson4/002/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10*/
+
+int MyNumb = ReadNumb();
+int a = MyNumb;
+int x = 0;
+int b = 0;
+
+int ReadNumb(){
+    System.Console.Write($"Введите число: ");
+    int x = int.Parse(System.Console.ReadLine());
+    return x;
+}
+
+for (int i = -10; i <= a; i++){
+    b = a % 10;
+    a /= 10;
+    x += b;
+}
+
+System.Console.WriteLine($"Сумма цифр числа {MyNumb} равна: {x}");
