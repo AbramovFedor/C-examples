e19985ef524de06a83a2d70eb7c10d2799ac63f8
Fedor Abramov <fedo2002abro@gmail.com>
2023-04-11 18:59:56 +0300

    Finished and added DzToRealLesson4 001

--- Examples/DzToRealLesson4/001/Program.cs
+++ Examples/DzToRealLesson4/001/Program.cs
@@ -2,3 +2,15 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16*/
 
+int ReadNumb(){
+    System.Console.Write($"Введите число: ");
+    int x = int.Parse(System.Console.ReadLine());
+    return x;
+}
+
+int a = ReadNumb();
+
+int b = ReadNumb();
+
+System.Console.Write($"Квадрат числа {a} в степени {b}: ");
+System.Console.WriteLine(Math.Pow(a,b));
