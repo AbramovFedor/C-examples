24d7cc2eb78eff6f2733c2ff2bbe3b805c9db0ce
Fedor Abramov <fedo2002abro@gmail.com>
2023-04-17 21:29:56 +0300

    Finished end added DzToLesson5

--- Examples/DzToLesson5/002/Program.cs
+++ Examples/DzToLesson5/002/Program.cs
@@ -1,19 +1,34 @@
 ﻿/*Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
-[3, 7, 23, 12] -> 19*/
+[3, 7, 23, 12] -> 19
+*/
 
 int [] numbers = CreateArray();
 PrintArray(numbers);
+SumOfNotEven(numbers);
 
 int [] CreateArray(){
     int a = new Random().Next(1,10);
     int [] numbers = new int[a];
-    System.Console.WriteLine(a);
 
     for (int i = 0; i < numbers.Length; i++){
-        numbers[i] = new Random().Next(100,1000);
+        numbers[i] = new Random().Next(1,10);
     }
     return numbers;
+}
+
+void SumOfNotEven(int[] numbers){
+    int i = 0;
+    int negative = 0;
+    while (i < numbers.Length){
+        if (numbers[i] % 2 == 0){
+            i++;
+        }else{
+            negative += numbers[i];
+            i++;
+        }
+    }System.Console.WriteLine($"Сумма нечетных чисел в массиве равна: {negative}");
+
 }
 
 void PrintArray(int[] numbers){
@@ -23,3 +38,4 @@ void PrintArray(int[] numbers){
     }
     System.Console.WriteLine(numbers[numbers.Length -1] + "]");
 }
+
---
+++ Examples/Lesson8/Lesson8Ex001/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте двумерный массив. Напишите программу,
+//которая поменяет местами первую и последнюю строку массива.
+
+int[,] CreateArray(int length1, int lenght2)
+{
+    int[,] Array = new int[length1, lenght2];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i, j] = new Random().Next(-10, 10);
+        }
+    }
+    return Array;
+
+}
+
+    void PrintArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            System.Console.Write(Array[i, j] + "\t");
+
+        }
+        System.Console.WriteLine();
+    }
+} 
+int [,] array=CreateArray(3,3);
+
+PrintArray(array);
+void ChancheString(int [,] array)
+{
+
+
+
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+       int temp = array [0,j];
+       array [0, j] = array[array.GetLength(0)-1,j];
+       array[array.GetLength(0)-1, j] = temp;
+    }
+
+System.Console.WriteLine();
+}
+ChancheString(array);
+PrintArray(array);
