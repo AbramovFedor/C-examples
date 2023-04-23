34f1f8d1b5b2888e480a64b4137eba774c093cb4
Fedor Abramov <fedo2002abro@gmail.com>
2023-04-23 12:25:55 +0300

    Finished and added Dz6

--- Examples/DzToLesson6/001/Program.cs
+++ Examples/DzToLesson6/001/Program.cs
@@ -1,2 +1,39 @@
 ﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 0, 7, 8, -2, -2 -> 2*/
+int lenght = Numbers("Введте количество элементов >");
+int[] array;
+array = InputArray(lenght);
+PrintArray(array);
+System.Console.WriteLine($"Количество чисел болше 0 = {CountPositiveNumbers(array)}");
+
+
+int Numbers(string message){
+    System.Console.WriteLine(message);
+    string value = System.Console.ReadLine();
+    int chenge = Convert.ToInt32(value);
+    return chenge;
+}
+
+int[] InputArray(int lenght){
+    int[] array = new int[lenght];
+    for (int i = 0; i < array.Length; i++){
+        array[i] = Numbers($"Введите {i + 1}-й элемент");
+    }
+    return array;
+}
+
+void PrintArray(int[] array){
+    for (int i = 0; i < array.Length; i++){
+        System.Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+
+int CountPositiveNumbers(int[] array){
+    int count = 0;
+    for (int i = 0; i < array.Length; i++){
+        if (array[i] > 0){
+            count++;
+        }
+    }
+    return count;
+}
