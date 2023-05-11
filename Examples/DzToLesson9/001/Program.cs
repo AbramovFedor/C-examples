1b75678eb60c64594c11d26267d91a652c22fa64
Fedor Abramov <fedo2002abro@gmail.com>
2023-05-11 18:08:20 +0300

    Added DzToLesson9 001

--- Examples/DzToLesson9/001/Program.cs
+++ Examples/DzToLesson9/001/Program.cs
@@ -2,5 +2,22 @@
 в промежутке от N до 1. Выполнить с помощью рекурсии.
 
 N = 5 -> "5, 4, 3, 2, 1"*/
+
 System.Console.Write("Ведите число: ");
-System.Console.ReadLine();
+int number = int.Parse(System.Console.ReadLine()!);
+
+ShowNumbers(number);
+
+void ShowNumbers(int number)
+{
+    if (number > 0)
+    {
+        System.Console.Write(number);
+        if (number != 1)
+        {
+            System.Console.Write(", ");
+        }
+        ShowNumbers(number -1);
+    }
+}
+
